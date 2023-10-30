79b233f7f042be0b4414859c0b70375ed82dc106
magonazz1 <martinmagonazz@outlook.com>
2023-10-29 19:51:28 -0700

    up - link

--- factors.py
+++ factors.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-
 import sys
 
-def factorize(number):
+def factorize(n):
     factors = []
-    for i in range(2, number + 1):
-        while number % i == 0:
-            factors.append(i)
-            number = number // i
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            factors.append(divisor)
+            n = n // divisor
+        else:
+            divisor += 1
     return factors
 
 def main(input_file):
-    with open(input_file, 'r') as file:
-        for line in file:
-            number = int(line)
-            factors = factorize(number)
-            if len(factors) == 2:
-                print(f"{number}={factors[0]}*{factors[1]}")
+    try:
+        with open(input_file, 'r') as file:
+            for line in file:
+                number = int(line)
+                factors = factorize(number)
+                factors_str = ' * '.join(map(str, factors))
+                print(f"{number}={factors_str}")
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found.")
+    except ValueError:
+        print("Error: Input file contains an invalid number.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-if __name__ == "__main__":
+if __name__ == "__main":
     if len(sys.argv) != 2:
         print("Usage: factors <file>")
         sys.exit(1)
-    
+
     input_file = sys.argv[1]
     main(input_file)
